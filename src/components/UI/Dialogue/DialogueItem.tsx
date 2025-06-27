3f3710a5636e8a9df8a763d977292ab248363441
Rbdizygotic <robnoycemars@gmail.com>
2025-06-27 15:11:45 -0400

    add the small disalogue

---
+++ src/components/UI/Dialogue/DialogueItem.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const DialogueItem: React.FC = ({img, name }) => {
+    return <div>
+    </div>
+}
+
+export default DialogueItem;
---
+++ src/components/UI/Dialogue/SmallDailogue.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import
+
+const SmallDailogue: React.FC = () => {
+    return <div className="fixed top-[60px] left-[16px] w-full h-full bg-white rounded-[8px] shadow-[4px_4px_32px_0px_rgba(0,0,0,0.08)]">
+    </div>
+}
+
+export default SmallDailogue;
