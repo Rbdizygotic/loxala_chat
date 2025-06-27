81b2433b3033762bc48cbb37d6c7faf7aae6e0a7
Rbdizygotic <robnoycemars@gmail.com>
2025-06-27 13:17:26 -0400

    fix the issue of the responsive

--- src/layouts/ChatList.tsx
+++ src/layouts/ChatList.tsx
@@ -22,14 +22,21 @@ const ChatList: React.FC<{ selectedChat: string | null, setSelectedChat: (chat: 
     console.log(result);
 
     return <div className='w-full h-full py-4'>
-        {!isShowChatBoard && <div className="flex w-full items-center justify-between mb-3 px-4">
+        {isMobile && !isShowChatBoard && <div className="flex w-full items-center justify-between mb-3 px-4">
             <span className='font-mulish text-[24px] font-bold leading-normal text-[#282529]'>Chats</span>
             <img src={Setting} className="w-5 h-5 cursor-pointer" />
         </div>}
-        {!isShowChatBoard &&
+        {isMobile && !isShowChatBoard &&
             <div className="px-4 mb-2">
                 <SearchInput placeholder="Search for a contact..." />
             </div>}
+        <div className="xs:flex hidden w-full items-center justify-between mb-3 px-4">
+            <span className='font-mulish text-[24px] font-bold leading-normal text-[#282529]'>Chats</span>
+            <img src={Setting} className="w-5 h-5 cursor-pointer" />
+        </div>
+        <div className="xs:block hidden px-4 mb-2">
+                <SearchInput placeholder="Search for a contact..." />
+            </div>
         <div className="w-full h-[calc(100%-90px)] py-4 overflow-y-auto">
             {result.map((item, key) => (
                 <ContactItem key={key} item={item} selectedChat={selectedChat} setSelectedChat={setSelectedChat} />
--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -12,7 +12,7 @@ const Chat: React.FC = () => {
 
 
     useEffect(() => {
-        const checkMobile = () => setIsMobile(window.innerWidth < 640);
+        const checkMobile = () => setIsMobile(window.innerWidth < 500);
         checkMobile();
         window.addEventListener('resize', checkMobile);
         return () => window.removeEventListener('resize', checkMobile);
@@ -20,47 +20,47 @@ const Chat: React.FC = () => {
 
     return <div className={`${isMobile ? (showChatBoard ? "p-0" : "p-4") : "p-6"} bg-[#f5f5f5]  xs:h-full h-[calc(100vh-40px)]`}>
         <div className={`w-full fixed top-0 right-0 sm:hidden flex justify-end items-center h-[40px] px-4 z- ${isMobile ? (showChatBoard ? "bg-white" : "bg-[#f5f5f5]") : "bg-[#f5f5f5]"}`}>
-        <div className="w-[36px] h-[36px] flex justify-center items-center  border border-[#EBEBEB] cursor-pointer rounded-[8px]">
-            <img src={NavtigationIcon} className="w-[20px] h-[20px]" />
+            <div className="w-[36px] h-[36px] flex justify-center items-center  border border-[#EBEBEB] cursor-pointer rounded-[8px]">
+                <img src={NavtigationIcon} className="w-[20px] h-[20px]" />
+            </div>
         </div>
-    </div>
-    {
-        showChatBoard && <div onClick={() => setShowChatBoard(false)} className='sm:hidden fixed top-[9px] z-1 left-4 h-[22px] flex items-center'>
-            <img src={Back} className='w-[16px] h-[16px] cursor-pointer' />
-            <span className='font-mulish text-[16px] font-bold leading-normal text-[#7A58D0]'>Back</span>
-        </div>
-    }
-    <div className={`flex bg-white ${isMobile ? (showChatBoard ? "rounded-none" : "rounded-[16px] border-[#EBEBEB] border ") : "rounded-[16px] border-[#EBEBEB] border"} h-full overflow-hidden`}>
-        {/* ChatList */}
-        <div
-            className={`
+        {
+            showChatBoard && <div onClick={() => setShowChatBoard(false)} className='sm:hidden fixed top-[9px] z-1 left-4 h-[22px] flex items-center'>
+                <img src={Back} className='w-[16px] h-[16px] cursor-pointer' />
+                <span className='font-mulish text-[16px] font-bold leading-normal text-[#7A58D0]'>Back</span>
+            </div>
+        }
+        <div className={`flex bg-white ${isMobile ? (showChatBoard ? "rounded-none" : "rounded-[16px] border-[#EBEBEB] border ") : "rounded-[16px] border-[#EBEBEB] border"} h-full overflow-hidden`}>
+            {/* ChatList */}
+            <div
+                className={`
                 transition-all duration-300 ease-in-out
                 ${isMobile ? (showChatBoard ? "w-0" : "w-full border-r border-[#EBEBEB]") : "w-[365px]  border-r border-[#EBEBEB]"}
                 xs:w-[365px] xs:block rounded-l-[16px]
                 `}
-        >
-            <ChatList
-                selectedChat={selectedChat}
-                isMobile={isMobile}
-                isShowChatBoard={showChatBoard}
-                setSelectedChat={(chat) => {
-                    setSelectedChat(chat);
-                    setShowChatBoard(true); // trigger switch to board
-                }}
-            />
-        </div>
+            >
+                <ChatList
+                    selectedChat={selectedChat}
+                    isMobile={isMobile}
+                    isShowChatBoard={showChatBoard}
+                    setSelectedChat={(chat) => {
+                        setSelectedChat(chat);
+                        setShowChatBoard(true); // trigger switch to board
+                    }}
+                />
+            </div>
 
-        {/* ChatBoard */}
-        <div
-            className={`
+            {/* ChatBoard */}
+            <div
+                className={`
                 transition-all duration-300 ease-in-out]
                 xs:${showChatBoard ? "w-full rounded-r-[16px]" : "w-0"}
                 xs:w-[calc(100%-365px)] xs:block
                 `}
-        >
-            <ChatBoard selectedChat={selectedChat} />
+            >
+                <ChatBoard selectedChat={selectedChat} />
+            </div>
         </div>
-    </div>
     </div >
 };
 
